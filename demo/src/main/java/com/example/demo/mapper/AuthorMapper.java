921e8a63bbf6a6a895a9acfc4bea776d893ff2d6
Assaf <1214assaf@gmail.com>
2024-07-03 13:56:31 +0300

    feat: Add AuthorMapper and BookMapper interfaces
    These interfaces, AuthorMapper and BookMapper, are added to the project. They use MapStruct to automate the conversion between entities and DTOs. The interfaces include methods for converting entities to DTOs and vice versa. This addition improves the codebase by providing a standardized and automated way to handle the conversion process.
    
    Related user commits:
    - Add AuthorModel and BookModel classes
    - Add Book entity
    - Add Author entity
    - Add ExternalBooksService for searching books by title

---
+++ demo/src/main/java/com/example/demo/mapper/AuthorMapper.java
@@ -0,0 +1,20 @@
+package com.example.demo.mapper;
+
+import com.example.demo.dto.AuthorDTO;
+import com.example.demo.model.AuthorModel;
+import com.example.demo.entity.Author;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+/**
+ Mappers: Convert entities to DTOs and vice versa.
+ BookMapper and AuthorMapper use MapStruct to automate this conversion.
+ */
+@Mapper
+public interface AuthorMapper {
+    AuthorMapper INSTANCE = Mappers.getMapper(AuthorMapper.class);
+
+    AuthorDTO toDTO(Author author);
+    Author toEntity(AuthorDTO authorDTO);
+    AuthorModel toModel(Author author);
+}
